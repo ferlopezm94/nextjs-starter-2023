f6d6670144edee433ef72dad0999065aff6e40e5
Fernando López <ferlopezm94@gmail.com>
2023-03-21 22:12:28 -0600

    Setup tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home(): JSX.Element {
         <link rel='icon' href='/favicon.svg' />
       </Head>
 
-      <h1>
+      <h1 className='text-3xl font-bold uppercase text-blue-600'>
         Welcome to <a href='https://nextjs.org'>Next.js!</a>
       </h1>
     </div>
